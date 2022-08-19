d056753c1ac91bfb0071b73a9ac93c85bdbeed9a
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-20 02:16:14 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/Boolean_1_불리언으로 변환.js
@@ -0,0 +1,14 @@
+/**
+ * - Boolean
+ *  On: True
+ *  Off: False
+ */
+
+console.log(Boolean('STRING')) // true
+console.log(!!'STRING') // true
+
+console.log(!false) // true
+console.log(!!false) // false
+console.log(!!0) // false
+console.log(!!1) // true
+
