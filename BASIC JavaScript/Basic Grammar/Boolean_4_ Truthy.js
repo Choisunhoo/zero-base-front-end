2ac76b363d48bdb7fc5c17ccc3b85be9cb9dcafa
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-20 12:40:56 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/Boolean_4_ Truthy.js
@@ -0,0 +1,41 @@
+/**
+ * Truthy
+ * 
+ */
+
+if (true) {
+    console.log('참입니다');
+}
+if ({}) {
+    console.log('참입니다');
+}
+if ([]) {
+    console.log('참입니다');
+}
+if (42) {
+    console.log('참입니다');
+}
+if ("0") {
+    console.log('참입니다');
+}
+if ("false") {
+    console.log('참입니다');
+}
+if (new Date()) {
+    console.log('참입니다');
+}
+if (-42) {
+    console.log('참입니다');
+}
+if (3.14) {
+    console.log('참입니다');
+}
+if (-3.14) {
+    console.log('참입니다');
+}
+if (Infinity) {
+    console.log('참입니다');
+}
+if (-Infinity) {
+    console.log('참입니다');
+}
