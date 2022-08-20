18bda79de62d92a73f95611ea434b9588b9f9e5c
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-20 12:58:31 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/Number_1_ Literal.js
@@ -0,0 +1,15 @@
+let Inf = Number.MAX_SAFE_INTEGER;
+
+console.log(Inf);  // 9007199254740991
+
+let num_1 = 35;
+console.log(num_1);  // 35
+
+let num_2 = 3.141;
+console.log(num_2);  // 3.141
+
+let num_3 = 0xFF;
+console.log(num_3);   // 255 
+
+let num_4 = 5e2;
+console.log(num_4);   // 500 
