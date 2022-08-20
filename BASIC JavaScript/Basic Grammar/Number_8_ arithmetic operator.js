2e459aa2d7a95083ecd08bb7f142a3847199d034
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-21 00:02:40 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/Number_8_ arithmetic operator.js
@@ -0,0 +1,25 @@
+/**
+ * 산술 연산자
+ * + - / * %
+ */
+
+console.log(1 + 1); //  2
+console.log(2 - 1); //  1
+console.log(10 / 5); //  2
+console.log(10 % 2); //  0
+console.log(2 * 2); //  4
+
+
+let x = 5;
+--x; 
+console.log(x); // 4
+x++;
+console.log(x); // 5
+x = x + 1;
+console.log(x); // 6
+x += 1;
+console.log(x); // 7
+
+console.log(3 + 10 * 2); // 23
+console.log(3 + (10 * 2)); // 23
+console.log((3 + 10) * 2); // 26
