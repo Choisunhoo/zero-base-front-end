1d048cea789e34c73d3394ccc5b7e73212394b0e
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-20 12:58:44 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/Number_2_ 숫자로변환.js
@@ -0,0 +1,8 @@
+
+console.log(Number(undefined)); // NaN
+console.log(Number(null)); // 0
+console.log(Number('')); // 0
+console.log(Number('String')); // NaN
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0
+console.log(Number(3.141592)); // 3.141592
