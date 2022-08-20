c3587d2b8c3d51b64d85b82d603f364028057d6b
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-20 12:33:53 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/Boolean_2_Comparison Operator.js
@@ -0,0 +1,14 @@
+/**
+ * == vs === (비교 연산자)
+ * 
+ */
+
+ console.log('1' == 1) // true 
+console.log(0 == []) // true 
+console.log(0 == '') // true
+console.log('' == []) // true
+
+console.log('1' === 1) // false 
+console.log(0 === []) // false 
+console.log(0 === '') // false
+console.log('' === []) // false
