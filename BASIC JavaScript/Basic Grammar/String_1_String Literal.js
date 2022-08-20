004afab2d6ce0fa09d577e219b252bb1405b7f20
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-21 00:28:18 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/String_1_String Literal.js
@@ -0,0 +1,21 @@
+/**
+ * 문자열 리터럴 
+ */
+
+
+const singleQuote = '';
+const doubleQuote = "1\
+2\
+3\
+4\
+";
+console.log(doubleQuote);
+const backQuote = `1
+2
+3
+4
+5`;
+console.log(backQuote);
+
+
+const empty = '';
