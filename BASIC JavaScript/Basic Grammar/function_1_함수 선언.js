e7dec2dd26780c893222cf62146bc851ef2beb3d
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-22 11:48:12 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/function_1_함수 선언.js
@@ -0,0 +1,27 @@
+/**
+ * 함수 선언
+ */
+
+
+// 함수 선언문 
+function sum(param1, param2) {
+    return param1 + param2
+}
+
+// 메서드 
+const obj = {
+    prop: function() {
+
+    }
+}
+
+// 생성자 함수 
+function MyObj() {
+
+}
+
+// 화살표 함수 (ES2015+. ES6)
+const  arrowFunc = () => {}
+
+
+consolo.log(sum(1, 2))
