76863ba0b7483d8e315c6d8183279b466b0d39c1
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-08-23 09:06:14 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/object_2_프로퍼티 열거.js
@@ -0,0 +1,21 @@
+/**
+ * 프로퍼티 열거
+ */
+
+const obj = {
+    prop1: 'value1',
+    prop2: 'value2',
+    prop3: 'value3',
+    prop4: 'value4',
+};
+
+const prop = 'prop';
+
+obj.prop1
+obj[prop + 1]
+
+for (const key in obj) {
+    if (obj.hasOwnProperty(key)) {
+        console.log(obj[key]);
+    }
+}
