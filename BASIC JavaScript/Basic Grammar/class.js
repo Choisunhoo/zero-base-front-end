ec41af11d05211575e229143ade6e15d5e1dcf1c
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-09-15 23:21:31 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/class.js
@@ -0,0 +1,17 @@
+/**
+ * class(글래스)
+ */
+
+function Person(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+}
+
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
