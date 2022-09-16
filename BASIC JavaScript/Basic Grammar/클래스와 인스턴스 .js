252a604f42bcf7f9c0adeed2470cae72191745d4
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-09-16 17:45:42 +0900

    Add files via upload

--- BASIC JavaScript/Basic Grammar/클래스와 인스턴스 .js
+++ BASIC JavaScript/Basic Grammar/클래스와 인스턴스 .js
@@ -1,24 +1,40 @@
 /**
- * 클래스와 인스턴스 
+ * 클래스 확장 
  */
 
 
-
-class Person {
-    constructor(name, age, location) {
+class Animal {
+    constructor(name, sound) {
         this.name = name;
-        this.age = age;
-        this.location = location;
+        this.sound = sound;
     }
 
-    getName() {
-        return this.name + '입니다';
-    };
+    getInfo() {
+        return (
+            this.name + '가(이)' + this.sound + '소리를 낸다.'
+        );
+    }
 }
 
+class Friends extends Animal {
+    constructor(name, sound) {
+        super(name, sound);
+    }
+}
 
-const me = new Person('jang', 10, 'Korea');
-const you = new Person('kim', 20, 'China');
+const dog = new Friends('개', '멍멍');
+const cat = new Friends('고양이', '냐옹');
 
-console.log(me.getName());
-console.log(you.getName());
+console.log(dog.getInfo()); //  개가(이)멍멍소리를 낸다.
+console.log(cat.getInfo()); //  고양이가(이)냐옹소리를 낸다.
+
+
+console.log(dog.constructor.name); // Friends
+console.log(cat.constructor.name); // Friends
+
+
+console.log(dog instanceof Friends); // true
+console.log(dog instanceof Animal); // true
+
+console.log(cat instanceof Friends); // true
+console.log(cat instanceof Animal); // true
