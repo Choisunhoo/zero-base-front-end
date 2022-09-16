8dc75d163cc5e3aa65184ca1cd7501fab965d4bb
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-09-16 17:23:35 +0900

    Add files via upload

---
+++ BASIC JavaScript/Basic Grammar/인스턴스.js
@@ -0,0 +1,18 @@
+/**
+ * 인스턴스란
+ */
+
+const me = {
+    name: 'jang',
+    age: 10,
+    location: 'korea',
+};
+
+function Func() {
+
+}
+
+class Class {}
+
+const NewInstance = new Func();
+const NewInstance2 = new Class(); 
