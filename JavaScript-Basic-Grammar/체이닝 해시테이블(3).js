fdaed1469c4b625fd7c2ec4431f6f441a15ca65e
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-09-20 16:05:13 +0900

    Add files via upload

---
+++ JavaScript-Basic-Grammar/체이닝 해시테이블(3).js
@@ -0,0 +1,239 @@
+/* 연결 리스트 (Linked List)
+각 노드가 데이터와 포인터를 가지며, 한 줄로 연결되어 있는 방식으로 데이터를 저장하는 자료 구조
+구현 메서드 (method)
+- 노드 개수 / 비어 있는지 확인 / 노드 출력: LinkedList.size(), LinkedList.isEmpty(), LinkedList.printNode()
+- 노드 추가: LinkedList.append(), LinkedList.insert()
+- 노드 삭제: LinkedList.remove(), LinkedList.removeAt()
+- 데이터 위치 확인: LinkedList.indexOf()
+*/
+
+/* 연결리스트 구현(4)
+*/ 
+
+// Node(): data와 point를 가지고 있는 객체 
+function Node(data) {
+    this.data = data; 
+    this.next = null;
+  }
+  
+  // LinkedList(): head와 length를 가지고 있는 객체 
+function LinkedList() {
+    this.head = null;
+    this.length = 0;
+  }
+  
+  // size(): 연결 리스트 내 노드 개수 확인 
+  LinkedList.prototype.size = function() {
+    return this.length;
+  };
+  
+  // isEmpty(): 객체 내 노드 존재 여부 파악 
+  LinkedList.prototype.isEmpty = function () {
+    return this.length === 0;
+  };
+  
+  // printNode(): 노드 출력 
+  LinkedList.prototype.printNode = function () {
+    for (let node = this.head; node != null; node = node.next) {
+      process.stdout.write(`${node.data} -> `);
+    } 
+    console.log("null");
+  }; 
+  
+  // append(): 연결 리스트 가장 끝에 노드 추가
+  LinkedList.prototype.append = function (value) {
+    let node = new Node(value),
+      current = this.head;
+  
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+  
+    this.length;
+  };
+  
+  // insert(): position 위치에 노드 추가 
+  LinkedList.prototype.insert = function (value, position = 0) {
+    if (position < 0 || position > this.length) {
+      return false;
+    }
+  
+    let node = new Node(value),
+      current = this.head,
+      index = 0,
+      prev;
+    
+    if (position === 0) {
+      node.next = current;
+      this.head = node;
+    } else {
+      while (index++ < position) {
+        prev = current;
+        current = current.next;
+      }
+  
+      node.next = current;
+      prev.next = node;
+    }
+  
+    this.length++;
+  
+    return true;
+  };
+  
+  // remove(): value 데이터를 찾아 노드 삭제
+  LinkedList.prototype.remove = function (value) {
+    let current = this.head,
+      prev = current;
+    
+    while (current.data != value && current.next != null
+  ) {
+      prev = current;
+      current = current.next;
+    }
+  
+    if (current.data != value) {
+      return null;
+    }
+  
+    if (current === this.head) {
+      this.head = current.next;
+    } else {
+      prev.next = current.next;
+    }
+  
+    this.length--;
+  
+    return current.data;
+  };
+  
+  // removeAt(): position 위치 노드 삭제 
+  LinkedList.prototype.removeAt = function (position = 0
+  ) { 
+    if (position < 0 || position >= this.length) {
+      return null;
+    }
+  
+    let current = this.head,
+      index = 0,
+      prev;
+    
+    if (position == 0) {
+      this.head = current.next;
+    } else {
+      while (index++ < position) {
+        prev = current;
+        current = current.next;
+      }
+  
+      prev.next = current.next;
+    }
+  
+    this.length--;
+  
+    return current.data;
+  };
+
+
+// 체이닝 해시테이블 
+
+const HASH_SIZE = 37; 
+
+// Element(): Key, value 저장을 위한 생성자
+function Element(key, value) {
+    this.key = key;
+    this.value = value;
+}
+
+// ChainingHashTable(): 생성자 
+function ChainingHashTable() {
+    this.table = new Array(HASH_SIZE);
+    this.length = 0;
+}
+
+// hashCode(): 해시 함수 
+ChainingHashTable.prototype.hashCode = function (key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+        hash += key.charCodeAt(i);
+    }
+    return hash % HASH_SIZE;
+};
+
+// clear(): 초기화 
+ChainingHashTable.prototype.clear = function () {
+    this.table = new Array(HASH_SIZE);
+    this.length = 0;
+};
+
+// size(): 크기 반환 
+ChainingHashTable.prototype.size = function () {
+    return this.length;
+};
+
+
+// 체이닝 해시테이블 구현(2)
+
+// put(): 데이터 추가 
+ChainingHashTable.prototype.put = function (key, value) {
+    let index = this.hashCode(key);
+    console.log(`key: ${key} -> index: ${index}`);
+
+    if (this.table[index] === undefined) {
+        this.table[index] = new LinkedList();
+    }
+
+    this.table[index].append(new Element(key, value));
+    this.length++;
+
+    return true;
+}
+
+// getBuffer(): 데이터 셋 반환 
+ChainingHashTable.prototype.getBuffer = function () {
+    let array = [];
+    for (let i = 0; i < this.table.length; i++) {
+        if (this.table[i]) {
+            let current = this.table[i].head;
+            do {
+                array.push(current.data);
+                current = current.next;
+            } while (current);
+        }
+    }
+    return array;
+};
+
+// print(): 데이터 셋 출력 
+ChainingHashTable.prototype.print = function () {
+    for (let i = 0; i < this.table.length; i++) {
+        if (this.table[i]) {
+            let current = this.table[i].head;
+            process.stdout.write(`#${i}`);
+            do {
+                process.stdout.write(` ->
+                    ${current.data.key}: ${current.data.value}`);
+                current = current.next;
+            } while (current);
+            console.log("");
+        }
+    };
+}
+
+
+let cht = new ChainingHashTable();
+cht.put("Ana", 172);
+cht.put("Donnie", 183); // collision
+cht.put("Sue", 163);
+cht.put("Jamie", 168); // collision
+cht.put("Paul", 190);
+
+console.log(cht);
+
+cht.print();
+console.log(cht.getBuffer());
