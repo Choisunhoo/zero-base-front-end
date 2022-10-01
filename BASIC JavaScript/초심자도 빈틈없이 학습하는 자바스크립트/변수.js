ea231f99aedf1ee8d0f62972f714cd6a9dcceeda
Choisunhoo <93267089+Choisunhoo@users.noreply.github.com>
2022-10-01 22:33:26 +0900

    Add files via upload

---
+++ BASIC JavaScript/초심자도 빈틈없이 학습하는 자바스크립트/변수.js
@@ -0,0 +1,25 @@
+// 변수: 분류된 데이터에 대하여 담을 수 잇는 이름을 가진 어떤 공간
+// 변수 선언 방식 - 데이터 종류 + 변수이름 + 세미콜론(;)
+
+
+// 변수 종류
+// var 가변형 변수 
+var animal;
+
+//  let 가변형 블록 지역 변수 
+let nation;
+
+// const 불변형 블록 지역 변수
+const planet = earth;
+
+
+// 할당: 공간에 데이터를 넣는 것 
+let animal = "cat";
+let nation = "korea";
+animal = "cat";
+nation = "korea";
+
+
+let country;
+country = "korea"; // 할당 
+country = "japan"; // 재할당
